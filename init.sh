2d34cfaa95db08686a55f98bcca51a0e44eef47b
undead <undead@notcomp.notcomp>
2025-09-08 13:09:06 +0300

    v0.0.2

--- init.sh
+++ init.sh
@@ -5,4 +5,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
+cat ./.zshrc > ~/.zshrc
+
+cat ./.p10k.zsh > ~/.p10k.zsh
+
+cat ./bashrc > ~/.bashrc
+
 
