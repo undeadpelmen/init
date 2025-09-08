0df48fb449ad659779111b9eed84f052147a69ca
undead <undead@notcomp.notcomp>
2025-09-08 12:45:22 +0300

    v0.0.1

---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo apt install curl git zsh snap 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+
